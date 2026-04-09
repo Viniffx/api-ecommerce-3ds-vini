c5db2e4e824c66e3e871c1f24ecc5e684fc963e4
viniffx <viniffx@gmail.com>
2026-04-09 10:23:54 -0300

    finalizado 09/04/26

--- src/main/java/br/com/senai/api_ecommerce/controller/CategoriaController.java
+++ src/main/java/br/com/senai/api_ecommerce/controller/CategoriaController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -22,28 +24,43 @@ public class CategoriaController {
     @Transactional
     public void cadastrarCategoria(@RequestBody @Valid DadosCadastroCategoria dados) {
         repository.save(new Categoria(dados));
-    };
+    }
+
+    ;
 
     @GetMapping
-    public Page<DadosListagemCategoria> listarCategorias(@PageableDefault(size=10, sort={"nome"}) Pageable paginacao){
+    public Page<DadosListagemCategoria> listarCategorias(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return repository.findAllByAtivoTrue(paginacao)
                 .map(DadosListagemCategoria::new);
     }
 
     @PutMapping
     @Transactional
-    public void atualizarCategoria(@RequestBody @Valid DadosAtualizarCategoria dados){
-        var categoria= repository.getReferenceById(dados.id());
+    public void atualizarCategoria(@RequestBody @Valid DadosAtualizarCategoria dados) {
+        var categoria = repository.getReferenceById(dados.id());
         categoria.atualizarCategoria(dados);
     }
 
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void deletarCategoria(@PathVariable Long id){
+    public void deletarCategoria(@PathVariable Long id) {
         var categoria = repository.getReferenceById(id);
         categoria.excluirCategoria();
     }
+
+    @GetMapping("/{id}")
+    public DadosDetalhamentoCategoria detalharCategoria(@PathVariable Long id){
+        Categoria categoria = repository.findByIdAndAtivoTrue(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Categoria não existe"));
+        return  new DadosDetalhamentoCategoria(categoria);
+
+    }
+
+}
+
 
    // @DeleteMapping("/{id}")
    // @Transactional
@@ -51,5 +68,5 @@ public class CategoriaController {
    //     repository.deleteById(id);
 //
    // }
-}
+
 
